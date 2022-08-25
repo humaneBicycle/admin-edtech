665613b1ee95942bf34fdc9f5ecae0b79ee12d92
abhaysood1793 <abhayasood99@gmail.com>
2022-08-25 19:08:32 +0530

    changed favicon

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
 
 export default function Event() {
   return (
-    <div>Event</div>
-  )
+    <>
+      <div className="row">
+        <div className="col-md-2 border-end">
+          <Navbar />
+        </div>
+        <div className="Navbar  d-flex justify-content-start mt-3 mb-4 border-bottom">
+            <div className="NavHeading ms-4">
+              <h2>Course</h2>
+            </div>
+
+            <div className=" ms-5 me-auto NavSearch">
+              <div className="input-group rounded d-flex flex-nowrap">
+                <input
+                  type="search"
+                  className="form-control rounded w-100"
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                />
+                <span className="input-group-text border-0" id="search-addon">
+                  <i className="fas fa-search"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        <div className="col-md-9">
+          fajdl
+          </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,8 +3,34 @@ import Navbar from '../components/Navbar'
 
 export default function Events() {
   return (
-    <div className='page-position-default'>
-        <Navbar/>
-    </div>
+    <div className="row">
+        <div className="col-md-2 border-end">
+          <Navbar />
+        </div>
+        
+        <div className="col-md-9">
+        <div className="Navbar  d-flex justify-content-start mt-3 mb-4 border-bottom">
+            <div className="NavHeading ms-4">
+              <h2>Events</h2>
+            </div>
+
+            <div className=" ms-5 me-auto NavSearch">
+              <div className="input-group rounded d-flex flex-nowrap">
+                <input
+                  type="search"
+                  className="form-control rounded w-100"
+                  placeholder="Search"
+                  aria-label="Search"
+                  aria-describedby="search-addon"
+                />
+                <span className="input-group-text border-0" id="search-addon">
+                  <i className="fas fa-search"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+       
+      </div>
+      </div>
   )
 }
